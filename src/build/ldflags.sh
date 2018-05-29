0478682bda2e6b266c0a18e62bfa69b11ba64562
andy <zhiangli029@gmail.com>
2018-05-29 14:18:53 +0800

    bugfix: build successfully with fatal error: No names found, cannot describe anything

--- src/build/ldflags.sh
+++ src/build/ldflags.sh
@@ -4,6 +4,6 @@ set -eu
 
 cd "$(dirname "${0}")/.."
 
-echo '-X "build.tag='$(git describe --tags)'"' \
+echo '-X "build.tag='$(git describe --tags --always)'"' \
      '-X "build.time='$(date -u '+%Y/%m/%d %H:%M:%S')'"' \
      '-X "build.git='$(git rev-parse --short HEAD)'"'
