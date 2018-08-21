5f5eb3c4e3508c4797aee9629ff92683a90e9fb0
andy <zhiangli029@gmail.com>
2018-08-21 18:06:05 +0800

    monitor: add the monitor test

--- src/monitor/monitor_test.go
+++ src/monitor/monitor_test.go
@@ -8,4 +8,29 @@
 
 package monitor
 
+import (
+	"testing"
 
+	dto "github.com/prometheus/client_model/go"
+	"github.com/stretchr/testify/assert"
+)
+
+func TestClientConnectionIncDec(t *testing.T) {
+	user := "andy"
+	ClientConnectionInc(user)
+
+	var m dto.Metric
+	g, _ := clientConnectionNum.GetMetricWithLabelValues(user)
+	g.Write(&m)
+	v := m.GetGauge().GetValue()
+
+	assert.EqualValues(t, 1, v)
+
+	ClientConnectionDec(user)
+
+	g, _ = clientConnectionNum.GetMetricWithLabelValues(user)
+	g.Write(&m)
+	v = m.GetGauge().GetValue()
+
+	assert.EqualValues(t, 0, v)
+}
