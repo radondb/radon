aea43df5005b9a3a8d1f8d5b4a478b3773342447
jasongao <gaoriyao@126.com>
2018-08-29 16:35:41 +0800

    xbase: remove func 'cancel()' as it will cause 'context cancel' error when use 'HTTPGet()'

--- src/xbase/http.go
+++ src/xbase/http.go
@@ -43,8 +43,7 @@ func makeSimpleRequest(ctx context.Context, method string, url string, payload i
 }
 
 func httpDo(method string, url string, payload interface{}) (*http.Response, func(), error) {
-	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
-	defer cancel()
+	ctx, _ := context.WithTimeout(context.Background(), 5*time.Second)
 	req, err := makeSimpleRequest(ctx, method, url, payload)
 	if err != nil {
 		return nil, nil, err
