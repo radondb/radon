7fd419f5effa8c37984dd20d28ff8a8d5117567b
zhyass <34016424+zhyass@users.noreply.github.com>
2018-09-14 12:20:45 +0800

    monitor: add query statistic for monitor

--- src/monitor/monitor_test.go
+++ src/monitor/monitor_test.go
@@ -34,3 +34,46 @@ func TestClientConnectionIncDec(t *testing.T) {
 
 	assert.EqualValues(t, 0, v)
 }
+
+func TestBackendConnectionIncDec(t *testing.T) {
+	address := "192.168.0.2:3306"
+	BackendConnectionInc(address)
+
+	var m dto.Metric
+	g, _ := backendConnectionNum.GetMetricWithLabelValues(address)
+	g.Write(&m)
+	v := m.GetGauge().GetValue()
+
+	assert.EqualValues(t, 1, v)
+
+	BackendConnectionDec(address)
+
+	g, _ = backendConnectionNum.GetMetricWithLabelValues(address)
+	g.Write(&m)
+	v = m.GetGauge().GetValue()
+
+	assert.EqualValues(t, 0, v)
+}
+
+func TestQueryTotalCounterInc(t *testing.T) {
+	command := "Select"
+	result := "OK"
+	QueryTotalCounterInc(command, result)
+	QueryTotalCounterInc(command, result)
+
+	var m dto.Metric
+	g, _ := queryTotalCounter.GetMetricWithLabelValues(command, result)
+	g.Write(&m)
+	v := m.GetCounter().GetValue()
+	assert.EqualValues(t, 2, v)
+
+	command = "Unsupport"
+	result = "Error"
+	QueryTotalCounterInc(command, result)
+
+	g, _ = queryTotalCounter.GetMetricWithLabelValues(command, result)
+	g.Write(&m)
+	v = m.GetCounter().GetValue()
+
+	assert.EqualValues(t, 1, v)
+}
