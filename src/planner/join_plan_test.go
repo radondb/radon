dc9b772a359ed5123e20dd6e6d68b920beb6b150
zhyass <34016424+zhyass@users.noreply.github.com>
2018-11-06 14:10:34 +0800

    planner: lift restrictions on the count of 'From'

--- src/planner/join_plan_test.go
+++ src/planner/join_plan_test.go
@@ -20,6 +20,7 @@ func TestJoinPlan(t *testing.T) {
 	querys := []string{
 		"select * from t where t.a=t.b",
 		"select x.id, y.id from x join y on x.id=y.id where x.id=1",
+		"select x.id, y.id from x,y where x.id=y.id",
 	}
 
 	log := xlog.NewStdLog(xlog.Level(xlog.PANIC))
@@ -34,30 +35,6 @@ func TestJoinPlan(t *testing.T) {
 			assert.Nil(t, err)
 			assert.Nil(t, plan.Children())
 			assert.Equal(t, "", plan.JSON())
-		}
-	}
-}
-
-func TestJoinUnsupportedPlan(t *testing.T) {
-	querys := []string{
-		"select x.id, y.id from x,y where x.id=y.id",
-	}
-	results := []string{
-		"unsupported: JOIN.expression",
-	}
-
-	log := xlog.NewStdLog(xlog.Level(xlog.PANIC))
-	for i, query := range querys {
-		tree, err := sqlparser.Parse(query)
-		assert.Nil(t, err)
-		node := tree.(*sqlparser.Select)
-		assert.Nil(t, err)
-		plan := NewJoinPlan(log, node)
-		{
-			err := plan.Build()
-			got := err.Error()
-			want := results[i]
-			assert.Equal(t, want, got)
 		}
 	}
 }
