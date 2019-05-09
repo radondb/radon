674902aba7a62babc0d40d506b273efda3a130cd
andy <zhiangli029@gmail.com>
2019-05-09 09:31:39 +0800

    proxy: add the privilege to kill #309

--- src/proxy/kill_test.go
+++ src/proxy/kill_test.go
@@ -14,6 +14,7 @@ import (
 	"testing"
 	"time"
 
+	"github.com/fortytw2/leaktest"
 	"github.com/stretchr/testify/assert"
 	"github.com/xelabs/go-mysqlstack/driver"
 	"github.com/xelabs/go-mysqlstack/sqlparser/depends/sqltypes"
@@ -82,6 +83,70 @@ func TestProxyKill(t *testing.T) {
 	// kill.
 	{
 		time.Sleep(time.Second * 1)
+		for i := 0; i < nums; i++ {
+			kill, err := driver.NewConn("mock", "mock", address, "test", "utf8")
+			assert.Nil(t, err)
+			wg.Add(1)
+			go func(c driver.Conn, id uint32) {
+				defer wg.Done()
+				query := fmt.Sprintf("kill %d", id)
+				_, err = kill.FetchAll(query, -1)
+				assert.Nil(t, err)
+			}(kill, clients[i].ConnectionID())
+		}
+	}
+	wg.Wait()
+}
+
+func TestProxyKillPrivilege(t *testing.T) {
+	defer leaktest.Check(t)()
+	log := xlog.NewStdLog(xlog.Level(xlog.PANIC))
+	fakedbs, proxy, cleanup := MockProxyPrivilegeUsers(log, MockDefaultConfig())
+	defer cleanup()
+	address := proxy.Address()
+
+	// fakedbs.
+	{
+		fakedbs.AddQueryPattern("use .*", &sqltypes.Result{})
+		fakedbs.AddQueryPattern("select * .*", &sqltypes.Result{})
+		fakedbs.AddQueryDelay("select * from test.t1_0002", &sqltypes.Result{}, 10000)
+		fakedbs.AddQueryDelay("select * from test.t1_0004", &sqltypes.Result{}, 10000)
+	}
+
+	var wg sync.WaitGroup
+	var clients []driver.Conn
+	nums := 2
+	// long query.
+	{
+		for i := 0; i < nums; i++ {
+			client, err := driver.NewConn("mock", "mock", address, "test", "utf8")
+			assert.Nil(t, err)
+			wg.Add(1)
+			go func(c driver.Conn) {
+				defer wg.Done()
+				query := "select * from t1"
+				_, err = client.FetchAll(query, -1)
+				log.Debug("%+v", err)
+			}(client)
+			clients = append(clients, client)
+		}
+	}
+
+	// kill.
+	{
+		time.Sleep(time.Second * 1)
+		for i := 0; i < nums; i++ {
+			kill, err := driver.NewConn("mock1", "mock", address, "test", "utf8")
+			assert.Nil(t, err)
+			wg.Add(1)
+			go func(c driver.Conn, id uint32) {
+				defer wg.Done()
+				query := fmt.Sprintf("kill %d", id)
+				_, err = kill.FetchAll(query, -1)
+				assert.NotNil(t, err)
+			}(kill, clients[i].ConnectionID())
+		}
+
 		for i := 0; i < nums; i++ {
 			kill, err := driver.NewConn("mock", "mock", address, "test", "utf8")
 			assert.Nil(t, err)
