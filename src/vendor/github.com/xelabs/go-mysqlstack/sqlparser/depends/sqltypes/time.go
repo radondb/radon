d0dc043596419ffda1b30d9443397f3505960d2e
zhyass <34016424+zhyass@users.noreply.github.com>
2019-08-16 20:11:30 +0800

    sqltypes: support time type conversion to numeric type #455

---
+++ src/vendor/github.com/xelabs/go-mysqlstack/sqlparser/depends/sqltypes/time.go
@@ -0,0 +1,127 @@
+/*
+ * go-mysqlstack
+ * xelabs.org
+ *
+ * Copyright (c) XeLabs
+ * GPL License
+ */
+
+package sqltypes
+
+import (
+	"errors"
+	"strconv"
+	"strings"
+
+	querypb "github.com/xelabs/go-mysqlstack/sqlparser/depends/query"
+)
+
+func TimeToNumeric(v Value) (numeric, error) {
+	switch v.Type() {
+	case querypb.Type_TIMESTAMP, querypb.Type_DATETIME:
+		var i int64
+		year, err := strconv.ParseInt(string(v.val[0:4]), 10, 16)
+		if err != nil {
+			return numeric{}, err
+		}
+		month, err := strconv.ParseInt(string(v.val[5:7]), 10, 8)
+		if err != nil {
+			return numeric{}, err
+		}
+		day, err := strconv.ParseInt(string(v.val[8:10]), 10, 8)
+		if err != nil {
+			return numeric{}, err
+		}
+		hour, err := strconv.ParseInt(string(v.val[11:13]), 10, 8)
+		if err != nil {
+			return numeric{}, err
+		}
+		minute, err := strconv.ParseInt(string(v.val[14:16]), 10, 8)
+		if err != nil {
+			return numeric{}, err
+		}
+		second, err := strconv.ParseInt(string(v.val[17:19]), 10, 8)
+		if err != nil {
+			return numeric{}, err
+		}
+
+		i = (year*10000+month*100+day)*1000000 + (hour*10000 + minute*100 + second)
+		if len(v.val) > 19 {
+			var f float64
+			microSecond, err := strconv.ParseUint(string(v.val[20:]), 10, 32)
+			if err != nil {
+				return numeric{}, err
+			}
+
+			microSec := float64(microSecond)
+			n := len(v.val[20:])
+			for n != 0 {
+				microSec *= 0.1
+				n--
+			}
+			f = float64(i) + microSec
+			return numeric{fval: f, typ: Float64}, nil
+		}
+		return numeric{ival: i, typ: Int64}, nil
+	case querypb.Type_DATE:
+		var i int64
+		year, err := strconv.ParseInt(string(v.val[0:4]), 10, 16)
+		if err != nil {
+			return numeric{}, err
+		}
+		month, err := strconv.ParseInt(string(v.val[5:7]), 10, 8)
+		if err != nil {
+			return numeric{}, err
+		}
+		day, err := strconv.ParseInt(string(v.val[8:]), 10, 8)
+		if err != nil {
+			return numeric{}, err
+		}
+		i = year*10000 + month*100 + day
+		return numeric{ival: i, typ: Int64}, nil
+	case querypb.Type_TIME:
+		var i int64
+		sub := strings.Split(string(v.val), ":")
+		if len(sub) != 3 {
+			return numeric{}, errors.New("incorrect.time.value,':'.is.not.found")
+		}
+
+		pre := int64(1)
+		if strings.HasPrefix(sub[0], "-") {
+			pre = -1
+			sub[0] = sub[0][1:]
+		}
+
+		hour, err := strconv.ParseInt(string(sub[0]), 10, 32)
+		if err != nil {
+			return numeric{}, err
+		}
+		minute, err := strconv.ParseInt(string(sub[1]), 10, 8)
+		if err != nil {
+			return numeric{}, err
+		}
+
+		if strings.Contains(sub[2], ".") {
+			second, err := strconv.ParseFloat(sub[2], 64)
+			if err != nil {
+				return numeric{}, err
+			}
+			f := float64(pre) * (float64(hour)*10000 + float64(minute)*100 + second)
+			return numeric{fval: f, typ: Float64}, nil
+		}
+
+		second, err := strconv.ParseInt(sub[2], 10, 8)
+		if err != nil {
+			return numeric{}, err
+		}
+		i = pre * (hour*10000 + minute*100 + second)
+		return numeric{ival: i, typ: Int64}, nil
+	case querypb.Type_YEAR:
+		val, err := strconv.ParseUint(v.ToString(), 10, 16)
+		if err != nil {
+			return numeric{}, err
+		}
+		return numeric{uval: val, typ: Uint64}, nil
+	}
+	return numeric{}, errors.New("unsupport: unknown.type")
+}
---
+++ src/vendor/github.com/xelabs/go-mysqlstack/sqlparser/depends/sqltypes/time_test.go
@@ -0,0 +1,83 @@
+/*
+ * go-mysqlstack
+ * xelabs.org
+ *
+ * Copyright (c) XeLabs
+ * GPL License
+ */
+
+package sqltypes
+
+import (
+	"reflect"
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func TestTimeToNumeric(t *testing.T) {
+	testcases := []struct {
+		in  Value
+		out interface{}
+	}{
+		{
+			in:  testVal(Timestamp, "2012-02-24 23:19:43"),
+			out: int64(20120224231943),
+		},
+		{
+			in:  testVal(Timestamp, "2012-02-24 23:19:43.120"),
+			out: float64(20120224231943.120),
+		},
+		{
+			in:  testVal(Time, "-23:19:43.120"),
+			out: float64(-231943.120),
+		},
+		{
+			in:  testVal(Time, "-63:19:43"),
+			out: int64(-631943),
+		},
+		{
+			in:  testVal(Datetime, "0000-00-00 00:00:00"),
+			out: int64(0),
+		},
+		{
+			in:  testVal(Datetime, "2012-02-24 23:19:43.000012"),
+			out: float64(20120224231943.000012),
+		},
+		{
+			in:  testVal(Date, "0000-00-00"),
+			out: int64(0),
+		},
+		{
+			in:  testVal(Date, "2012-02-24"),
+			out: int64(20120224),
+		},
+		{
+			in:  testVal(Year, "2012"),
+			out: uint64(2012),
+		},
+		{
+			in:  testVal(Year, "12"),
+			out: uint64(12),
+		},
+	}
+
+	for _, tcase := range testcases {
+		got, err := TimeToNumeric(tcase.in)
+		assert.Nil(t, err)
+
+		var v interface{}
+		switch got.typ {
+		case Uint64:
+			v = got.uval
+		case Float64:
+			v = got.fval
+		case Int64:
+			v = got.ival
+		}
+
+		if !reflect.DeepEqual(v, tcase.out) {
+			t.Errorf("%v.ToNative = %#v, want %#v", makePretty(tcase.in), v, tcase.out)
+		}
+	}
+}
